a7bf1c71e6501e25553bccd653dd683d32bfa64d
qwertytuan <tuanthai902@gmail.com>
2025-02-26 12:03:20 +0700

    Initial commit

---
+++ Models/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace ASP_NET_WEB.Models;
+
+public class DataContext 
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default")!);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
